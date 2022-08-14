3408a5353e1054a23358dfe6b996c6d845542185
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-14 15:44:14 +0600

    event handeler bg color

--- js-dom-event/js/events.js
+++ js-dom-event/js/events.js
@@ -1,1 +1,22 @@
 console.log('This is separate JS file');
+// option 1 directly put html element
+
+// option 2 bg change 
+function makeRed(){
+    document.body.style.backgroundColor = 'red';
+}
+
+
+// options 3
+const makeBlueButton = document.getElementById('make-blue');
+       makeBlueButton.onclick = makeBlue;
+
+       function makeBlue (){
+        document.body.style.backgroundColor = 'blue';
+       }
+
+// option 04
+const purpleButton = document.getElementById('make-purple');
+     purpleButton.onclick = function makePurple() {
+        document.body.style.backgroundColor = 'purple'
+     }
