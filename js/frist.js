67031fceb39938f80c0869bcc30ad637cb01cefe
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-14 14:48:36 +0600

    new section added with dom

---
+++ js/frist.js
@@ -0,0 +1,12 @@
+console.log('Second Pharagrah')
+
+const allLi = document.getElementsByTagName('li');
+console.log(allLi);
+
+const allTitles = document.getElementsByClassName('section-title');
+console.log(allTitles);
+
+const secondSection = document.getElementById('secondSection');
+console.log(secondSection);
+secondSection.style.color = 'white';
+secondSection.style.backgroundColor = 'blue'
---
+++ js/third.js
@@ -0,0 +1,15 @@
+const main = document.getElementById('main-container');
+const section = document.createElement('section');
+section.innerHTML = `
+<h1> My dynamic section </h1>
+<p>Extra text added inside paragraph</p>
+<ul>
+    <li>first item</li>
+    <li>first item</li>
+    <li>first item</li>
+    <li>first item</li>
+</ul>
+
+`
+
+main.appendChild(section)
