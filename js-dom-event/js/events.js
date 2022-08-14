89b1c964eb4bcfc1217debb151205de7a5226db6
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-14 16:17:01 +0600

    add event listener dom

--- js-dom-event/js/events.js
+++ js-dom-event/js/events.js
@@ -20,3 +20,25 @@ const purpleButton = document.getElementById('make-purple');
      purpleButton.onclick = function makePurple() {
         document.body.style.backgroundColor = 'purple'
      }
+
+// Option 5
+const pinkButton = document.getElementById('make-pink')
+pinkButton.addEventListener('click' , makePink)
+
+function makePink(){
+     document.body.style.backgroundColor = 'pink';
+}
+
+// Option 6
+
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click' , function makeGreen(){
+  document.body.style.backgroundColor = 'green'
+})
+
+// Option 7
+// important
+//   document.getElementById('make-goldenrod' , function(){
+    document.getElementById('make-goldenrod').addEventListener('click ' , function(){
+        document.body.style.backgroundColor = 'goldenrod';
+      })
