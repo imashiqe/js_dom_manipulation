2c5122cf365403df363b00dc2563182fe594009c
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-23 23:12:16 +0600

    note dom manipulation

--- dom_recap/recap.js
+++ dom_recap/recap.js
@@ -5,4 +5,22 @@
    2-2 :  getElementsByClassName
    2-3 : querySelector
    2-4 : querySelectorAll 
+
+   3. dynamically add elenebt to the dom : document.createElement
+     
+   3-5: set value to the innerHtml
+   4 , get value from/to a dom element
+     4- 5: use innerText (h1.p,h5, div, span, strong)
+     4-2: use value (input, textArea)
+   
+     5. add event lister ( event handler)
+       5-1: click, focus, blur, onchan ge, keyup
+       5-2: addEvenListener onClick="myHandler()"
+   6. use function to reduce duplicate code
+
+   7. event bubble and event delegate
 */
+
+document.querySelectorAll('.users ' , function(){
+    console.log(users)
+})
