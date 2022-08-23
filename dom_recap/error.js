9fc7124e7257214ae8d53c377c1bacfeaa10e1be
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-24 00:04:32 +0600

    try catch error js

---
+++ dom_recap/error.js
@@ -0,0 +1,18 @@
+// not in js that frequently: range error
+// const numbers = [3,4,5,8];
+// you should not do it (most of the time)
+// numbers.length = 2;
+// console.log(numbers);
+// console.log(numbers.length);
+
+
+//reference error
+
+//syntex error 
+// for(let i =0; i<5 i++){
+
+// }
+
+// undefined
+let student ={'address'};
+console.log(student.address.city)
---
+++ dom_recap/try-catch.js
@@ -0,0 +1,14 @@
+const day = 'day the din';
+console.log(day);
+console.log(25);
+console.log(district);
+let district = 'bBaria';
+
+console.log(100);
+console.log(100);
+console.log(100);
+
+try{
+    console.log('inside try');
+    console.log(error)
+}
