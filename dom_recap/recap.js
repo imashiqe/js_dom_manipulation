cf6e537747251bc2a95ff0d452dd42b5cb081651
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-23 22:48:09 +0600

    js recap

---
+++ dom_recap/recap.js
@@ -0,0 +1,8 @@
+/*
+1. how to use js with html and css
+2. get element or elements from dom
+   2-1 :  getElementById
+   2-2 :  getElementsByClassName
+   2-3 : querySelector
+   2-4 : querySelectorAll 
+*/
