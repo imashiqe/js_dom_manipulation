072706f5aa1b6fed26c9be24357cfb672f750c01
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-24 23:07:22 +0600

    template string  backtick

---
+++ Es-6-js/template.js
@@ -0,0 +1,27 @@
+const person = 'Adam Sandler';
+const person2 = "Ben White";
+const person3 = `Donald Trump`;
+
+const multiLine = 'First Line Text \n' +
+'second line of code \n' +
+'Third line of text' +
+'Fourth line of string';
+// console.log(multiLine)
+
+const newMultiLine = `First Line of code 
+second Liner of text
+third line of string
+fourth line of code`;
+
+// console.log(newMultiLine);
+
+
+const a = 20;
+const b = 30;
+const nums = [78,98,45,12,6444]
+const summary = 'sum of:' + a + 'and' + b + 'is' + (a+b);
+
+// console.log(summary)
+const newSummary = `sum of ${a} and ${b} is: ${a+b}`;
+console.log(newSummary);
+
