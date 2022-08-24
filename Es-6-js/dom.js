f0a40770ea7259ce92218b7ac21c9c877b1b93e6
Ashiqur Rahman <67426392+imashiqe@users.noreply.github.com>
2022-08-24 12:46:50 +0600

    node child append added

--- Es-6-js/dom.js
+++ Es-6-js/dom.js
@@ -9,3 +9,15 @@ document.getElementById('center-third').addEventListener('click' , function(){
     const third = document.getElementById('third-friend');
     third.style.textAlign = 'center';
 })
+
+document.getElementById('add-friend').addEventListener('click', function(){
+   const friendContainer = document.getElementById('friends');
+   const friend = document.createElement('div');
+   friend.classList.add('friend');
+   friend.innerHTML = `
+   
+   <h3 class="friend-name">New Friend</h3>
+   <p>Something New Added</p>
+   `
+   friendContainer.appendChild(friend);
+})
